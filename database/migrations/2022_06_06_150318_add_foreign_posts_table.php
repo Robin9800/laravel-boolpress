a3e6161f8d1165ae74bff763eba2c656e036d1a0
Robin9800 <robin.costanzo@gmail.com>
2022-06-06 17:15:58 +0200

    dropForeign added

--- database/migrations/2022_06_06_150318_add_foreign_posts_table.php
+++ database/migrations/2022_06_06_150318_add_foreign_posts_table.php
@@ -29,6 +29,16 @@ class AddForeignPostsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('posts', function(Blueprint $table){
+
+            $table->dropForeign('posts_category_id_foreign');
+            $table->dropColumn('category_id');
+
+            $table->unsignedBigInteger('category_id');
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories');
+        });
     }
 }
