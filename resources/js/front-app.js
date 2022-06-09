41ddfe66e4a39e3c4ffa9baa60bdf584e47abd2d
Robin9800 <robin.costanzo@gmail.com>
2022-06-09 16:39:46 +0200

    Vue fixed

--- resources/js/front-app.js
+++ resources/js/front-app.js
@@ -1,6 +1,12 @@
-AppComponent = require('./AppComponent.vue').default
+const { get } = require('jquery');
 
-const app = new Vue({
-    el: '#app',
-    render: (h)=> h(AppComponent)
-});
+require ('/bootstrap');
+
+windows.axios = require('axios');
+windows.axios.get('https://127.0.0.1:8000/api/posts').then(result=>{
+    console.log(results);
+}).catch(e=>{
+    console.log(e);
+})
+
+windows.Vue = require('vue');
