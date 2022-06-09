109bfb4507ec12ca11b1c5f45bd5df97ad8cac54
Robin9800 <robin.costanzo@gmail.com>
2022-06-09 18:59:22 +0200

    HomeComponent route added

---
+++ resources/js/pages/HomeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+      <div class="row">
+          <div class="col-12">
+              Home
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/routes.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    rputes:[
+        {
+            paths:'/',
+            name: 'home',
+            component: HomeComponent
+        }
+    ]
+})
