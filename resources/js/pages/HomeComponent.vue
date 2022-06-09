152d8fa17ebb1bbdafcbb8886d619cb9e4b79c9a
Robin9800 <robin.costanzo@gmail.com>
2022-06-09 19:30:30 +0200

    router-view added

--- resources/js/pages/HomeComponent.vue
+++ resources/js/pages/HomeComponent.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="container">
-      <div class="row">
-          <div class="col-12">
-              Home
-          </div>
-      </div>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
